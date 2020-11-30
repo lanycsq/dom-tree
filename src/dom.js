f9fe1461a78083f1b3ed811b71237c5a8df191d8
lanycsq <shanqiong118@163.com>
2020-11-30 10:29:44 +0800

    添加多个APII

--- src/dom.js
+++ src/dom.js
@@ -23,4 +23,125 @@ window.dom = {
             fn.call(null, nodeList[i])
         }
     },
+    create(string) {
+        const container = document.createElement("template");
+        container.innerHTML = string.trim();
+        return container.content.firstChild;
+    },
+    // 用于新增弟弟
+    after(node, node2) {
+        node.parentNode.insertBefore(node2, node.nextSibling);
+    },
+    //用于新增哥哥
+    before(node, node2) {
+        node.parentNode.insertBefore(node2, node);
+    },
+    //
+    append(parent, node) {
+        parent.appendChild(node)
+    },
+    //新增一个爸爸
+    wrap(node, parent) {
+        dom.before(node, parent)
+        dom.append(parent, node)
+    },
+    remove(node) {
+        node.parentNode.removeChild(node);
+        return node
+    },
+    empty(node) {
+        const {
+            childNodes
+        } = node;
+        const array = []
+        let x = node.firstChild;
+        while (x) {
+            array.push(dom.remove(node.firstChild))
+            x = node.firstChild;
+        }
+        return array
+    },
+    attr(node, name, value) {
+        if (arguments.length === 3) {
+            node.setAttribute(name, value)
+        } else if (arguments.length === 2) {
+            return node.getAttribute(name)
+        }
+    },
+    text(node, string) { //适配
+        if (arguments.length === 2) {
+            if ('innerText' in node) { //ie
+                node.innerText = string;
+            } else { //chrome
+                node.textContent = string;
+            }
+        } else if (arguments.length === 1) {
+            if ('innerText' in node) { //ie
+                return node.innerText
+            } else { //chrome
+                return node.textContent
+            }
+        }
+
+    },
+    html(node, string) {
+        if (arguments.length === 2) {
+            node.innerHTML = string
+        } else if (arguments.length === 1) {
+            return node.innerHTML
+        }
+    },
+
+    class: {
+        add(node, className) {
+            node.classList.add(className)
+        },
+        remove(node, className) {
+            node.classList.remove(className)
+        },
+        has(node, className) {
+            return node.classList.contains(className)
+        }
+    },
+    on(node, eventName, fn) {
+        node.addEventListener(eventName, fn)
+    },
+    off(node, eventName, fn) {
+        node.removeEventListener(eventName, fn)
+    },
+
+    parent(node) {
+        return node.parentNode
+    },
+    children(node) {
+        return node.children;
+    },
+    sublings(node) {
+        return Array.from(node.parentNode.children)
+    },
+    next(node) {
+        let x = node.nextSibling
+        while (x && x.nodeType === 3) {
+            x = x.nextSibling
+        }
+        return x
+    },
+    previous(node) {
+        let x = node.previousSibling
+        while (x && x.nodeType === 3) {
+            x = x.previousSibling
+        }
+        return x
+    },
+
+    index(node) {
+        const list = dom.children(node.parentNode)
+        let i
+        for (i = 0; i < list.length; i++) {
+            if (list[i] === node) {
+                break
+            }
+        }
+        return i
+    }
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,73 @@
-const div = dom.find('#test>.red')[0] // 获取对应的元素
-console.log(div);
-dom.style(div, 'color', 'red') // 设置 div.style.color
+// const div = dom.find('#test>.red')[0] // 获取对应的元素
+// console.log(div);             
+// dom.style(div, 'color', 'red') // 设置 div.style.color
 
-const divList = dom.find('.red') // 获取多个 div.red 元素
-console.log(divList);
-dom.each(divList, (n) => console.log(n)) // 遍历 divList 里的所有元素dom
+// const divList = dom.find('.red') // 获取多个 div.red 元素
+// dom.each(divList, (n) => console.log(n)) // 遍历 divList 里的所有元素dom
+
+//创建一个div标签
+const div = dom.create("<div>newDiv</div>")
+console.log(div);
+
+dom.after(test, div)
+
+const div3 = dom.create("<div id='parent'></div>")
+dom.wrap(test, div3)
+
+//清空一个div
+const nodes = dom.empty(window.empty)
+console.log(nodes);
+
+dom.attr(test, "title", "nihao")
+const title = dom.attr(test, "title")
+console.log(`title:${title}`);
+
+dom.text(test, "你好，这是新的内容")
+const textString = dom.text(test)
+console.log(textString);
+
+dom.html(test, "面向对象<p>封装</p>")
+const htmlString = dom.html(test)
+console.log(htmlString);
+
+
+dom.style(test, {
+    border: '1px solid red',
+    color: 'blue'
+})
+const testColor = dom.style(test, 'color')
+console.log(testColor);
+
+dom.style(test, 'font-size', '28px')
+dom.class.add(test, 'bgred')
+dom.class.remove(test, 'bgred')
+const ishas = dom.class.has(test, 'bgred')
+console.log(ishas);
+
+const fn = () => {
+    console.log('点击了');
+}
+dom.on(test, 'click', fn)
+dom.off(test, 'click', fn)
+
+const testDiv = dom.find('#test')[0]
+console.log(testDiv);
+const findDiv = dom.find('#find')[0]
+const find = dom.find('.red', findDiv)[0]
+console.log(find);
+console.log("-------------------");
+
+const S2 = dom.find("#s2")[0]
+console.log(dom.parent(S2));
+console.log(dom.sublings(S2));
+console.log("=============");
+console.log(dom.next(S2));
+console.log(dom.previous(S2));
+
+
+const t = dom.find("#travel")[0]
+dom.each(dom.children(t), (n) => {
+    dom.style(n, 'color', 'red')
+})
+
+console.log(dom.index(dom.find("#t2")[0]));
